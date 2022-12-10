b407001929888ec3792008783823f5345e21268c
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-12-10 23:38:05 +0200

    the function of uploading user avatars has been created

---
+++ src/utils/load-avatars.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { Buffer } from 'buffer';
+
+type ImageType = {
+	data: string;
+	status: number;
+	statusText: string;
+};
+
+export const loadAvatars = async (count: number) => {
+	const data: string[] = [];
+	const api = 'https://api.multiavatar.com/456789';
+	const randomNumber = Math.round(Math.random() * 1000);
+
+	try {
+		for (let i = 0; i < count; i++) {
+			const image: ImageType = await axios.get(`${api}/${randomNumber}`);
+			const buffer = new Buffer(image.data);
+			data.push(buffer.toString('base64'));
+		}
+
+		return data;
+	} catch (error) {
+		throw error;
+	}
+};
