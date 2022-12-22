98f4044c064809b6ac80a74694813ef2991aa37d
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-12-22 08:09:48 +0200

    use-auth hook has been completed

--- src/hooks/use-auth/use-auth.ts
+++ src/hooks/use-auth/use-auth.ts
@@ -1,25 +1,33 @@
 import { getUser } from './../../redux/services/user/selectors';
 import { useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../redux/store/hooks';
 
 export const useAuth = () => {
 	const navigate = useNavigate();
 	const user = useAppSelector(getUser);
 	const isAuth = Boolean(user);
-	// const pathname = window.location.pathname;
+	const pathname = window.location.pathname;
+
+	useEffect(() => {
+		if (user && !user.avatar) {
+			navigate('/set-avatar');
+		}
+	}, [navigate, isAuth, user]);
 
 	useEffect(() => {
 		if (isAuth) {
 			navigate('/');
 		}
 
-		// if (!isAuth && pathname === '/register') {
-		// 	navigate('/register');
-		// }
-
-		if (user && !user.avatar) {
-			navigate('/set-avatar');
+		if (!isAuth) {
+			navigate('/login');
 		}
-	}, [navigate, isAuth, user]);
+	}, [navigate, isAuth]);
+
+	useEffect(() => {
+		if (!isAuth && pathname === '/register') {
+			navigate('/register');
+		}
+	}, [navigate, isAuth, pathname]);
 };
